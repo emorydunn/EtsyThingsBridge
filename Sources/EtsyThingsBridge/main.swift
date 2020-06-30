32b15a8742035684688e4d51b3dbf8bd9100d740
Emory Dunn <edunn@emorydunn.com>
2020-06-30 16:21:58 -0700

    Add support for saving orders cache

--- Sources/EtsyThingsBridge/main.swift
+++ Sources/EtsyThingsBridge/main.swift
@@ -7,53 +7,113 @@
 
 import Foundation
 import EtsyThingsCore
+import MapKit
 
-if #available(OSX 10.12, *) {
-    let config = Config()
+
+let config = Config()
+
+var etsy: EtsyAuth!
+do {
+    etsy = try config.loadKeys()
+} catch {
+    print(error.localizedDescription)
     
-    var etsy: EtsyAuth!
     do {
-        etsy = try config.loadKeys()
+        let url = try config.writeConfig()
+        print("Writing empty keys file to \(url.path)")
+        
     } catch {
         print(error.localizedDescription)
-        
-        do {
-            let url = try config.writeConfig()
-            print("Writing empty keys file to \(url.path)")
-            
-        } catch {
-            print(error.localizedDescription)
-        }
-        
-        exit(EXIT_FAILURE)
     }
     
-    etsy.fetchOpenOrders { response in
+    exit(EXIT_FAILURE)
+}
+
+var allOrders: [Order] = []
+
+/// Download all order, repeating as necessary for pages
+/// - Parameters:
+///   - fetchTransactions: Fetch transactions for each order
+///   - limit: Paging limit
+///   - offset: Paging offset
+func downloadOrders(fetchTransactions: Bool = true, limit: Int? = nil, offset: Int? = nil) {
+    
+    etsy.fetchOpenOrders(fetchTransactions: false, limit: limit, offset: offset) { response in
         switch response {
         case .success(let orders):
-            print("Received \(orders.count) orders")
             
-            do {
-                try EtsyThingsBridge.makeProjects(for: orders, in: "Etsy")
-            } catch {
-                print(error.localizedDescription)
-                exit(EXIT_FAILURE)
+            allOrders.append(contentsOf: orders.results)
+            
+            print("Received \(allOrders.count) orders of \(orders.count)")
+            
+            
+            
+            if allOrders.count < orders.count {
+                let newOffset: Int?
+                if let limit = limit, let offset = offset {
+                    newOffset = limit + offset
+                } else {
+                    newOffset = nil
+                }
+                downloadOrders(fetchTransactions: fetchTransactions, limit: limit, offset: newOffset)
+            } else {
+                print("All orders downloaded: \(allOrders.count)")
+                
+                do {
+                    try saveOrders()
+                    openInMaps()
+                } catch {
+                    print(error)
+                }
+
             }
             
-            print("Project creation complete")
-            exit(EXIT_SUCCESS)
-        case .error(let e):
+            
+        
+        case .failure(let e):
             print(e)
             
             exit(EXIT_FAILURE)
         }
         
     }
-    
-    dispatchMain()
-
-    
-} else {
-    print("EtsyThingsBridge is only available on macOS 10.12 and newer")
-    exit(EXIT_FAILURE)
 }
+
+func openInMaps() {
+    etsy.fetchMapLocations(for: allOrders) { items in
+        print("Opening \(items.count) in Maps")
+        MKMapItem.openMaps(with: items, launchOptions: nil)
+        exit(EXIT_SUCCESS)
+    }
+}
+
+func saveOrders() throws {
+    let encoder = JSONEncoder()
+    let data = try encoder.encode(allOrders)
+    
+    try data.write(to: URL(fileURLWithPath: "/Users/emorydunn/Desktop/Orders.json"))
+}
+
+func loadOrders() throws {
+    let decoder = JSONDecoder()
+    let data = try Data(contentsOf: URL(fileURLWithPath: "/Users/emorydunn/Desktop/Orders.json"))
+    allOrders = try decoder.decode([Order].self, from: data)
+}
+
+
+//downloadOrders(fetchTransactions: false, limit: 100, offset: 0)
+
+do {
+    try loadOrders()
+    openInMaps()
+} catch {
+    print(error)
+}
+
+
+
+
+
+
+
+dispatchMain()
